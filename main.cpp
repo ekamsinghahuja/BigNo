ea471d7a5dc5676fc75b948595eefed175f21fd5
ekamsinghahuja <ekamsinghahuja123@gmail.com>
2024-06-30 17:33:04 +0530

    const issue

--- main.cpp
+++ main.cpp
@@ -9,9 +9,9 @@ public:
     bool isNeg;
 
 private:
-    string addString(const string& a, const string& b) {
+    string addString(const string& a, const string& b) const {
         string res;
-        int s1 = a.size(), s2 = b.size(),dig,carry = 0,i = 0, j = 0;
+        int s1 = a.size(), s2 = b.size(), dig, carry = 0, i = 0, j = 0;
         while (i < s1 && j < s2) {
             int dig = carry + (a[i++] - '0') + (b[j++] - '0');
             res.push_back((dig % 10 + '0'));
@@ -27,138 +27,122 @@ private:
             res.push_back((dig % 10 + '0'));
             carry = dig / 10;
         }
-        if (carry > 0)res.push_back((carry + '0'));
+        if (carry > 0) res.push_back((carry + '0'));
         return res;
     }
-    string subString(string& a,string& b){
+    string subString(const string& a, const string& b) const {
         string res;
-        int s1 = a.size(), s2 = b.size(),dig,bow = 0,i = 0, j = 0;
-        while(i<s1 && j<s2){
-            if(a[i]-bow < b[j]){
-                dig = (a[i++]-'0')+10-bow-(b[j++]-'0');
-                res.push_back((dig+'0'));
-                bow = 1; 
-            }
-            else{
-                dig = (a[i++]-'0')-bow-(b[j++]-'0');           
-                res.push_back((dig+'0'));
+        int s1 = a.size(), s2 = b.size(), dig, bow = 0, i = 0, j = 0;
+        while (i < s1 && j < s2) {
+            if (a[i] - bow < b[j]) {
+                dig = (a[i++] - '0') + 10 - bow - (b[j++] - '0');
+                res.push_back((dig + '0'));
+                bow = 1;
+            } else {
+                dig = (a[i++] - '0') - bow - (b[j++] - '0');
+                res.push_back((dig + '0'));
                 bow = 0;
             }
         }
-        while(i<s1){
-            if((a[i]-'0')-bow < 0){
-                dig = (a[i++]-'0')+10-bow;
-                res.push_back((dig+'0'));
-                bow = 1; 
-            }
-            else{
-                dig = (a[i++]-'0')-bow;
-                res.push_back((dig+'0'));
+        while (i < s1) {
+            if ((a[i] - '0') - bow < 0) {
+                dig = (a[i++] - '0') + 10 - bow;
+                res.push_back((dig + '0'));
+                bow = 1;
+            } else {
+                dig = (a[i++] - '0') - bow;
+                res.push_back((dig + '0'));
                 bow = 0;
             }
         }
         int ans_len = 0;
-        for(int i=res.size()-1;i>=0;i--){
-            if(res[i]!='0')break;
+        for (int i = res.size() - 1; i >= 0; i--) {
+            if (res[i] != '0') break;
             ans_len++;
         }
         ans_len = res.size() - ans_len;
-        if(ans_len==0)return "0";
-        return res.substr(0,ans_len);
+        if (ans_len == 0) return "0";
+        return res.substr(0, ans_len);
     }
-    bool absGreaterThan(string& num1,string& num2) {
+    bool absGreaterThan(const string& num1, const string& num2) const {
         if (num1.size() != num2.size()) {
             return num1.size() > num2.size();
         }
-        int i=num1.size()-1;
-        while(i>=0&&num1[i]==num2[i]){
+        int i = num1.size() - 1;
+        while (i >= 0 && num1[i] == num2[i]) {
             i--;
         }
-        if(i<0){
-            return 0;
+        if (i < 0) {
+            return false;
         }
         return num1[i] > num2[i];
     }
-    bool isNegative(){
+    bool isNegative() const {
         return isNeg;
     }
-public:
-    BigInteger(string s ,bool rev_true = false) {
-        if(rev_true){
-            int ss = s.size();
-            if (s[ss-1] == '-'){
-                this->isNeg = true; 
-                this->data = s.substr(0,ss-1); 
-            } 
-            else{
-                this->isNeg = false;
-                this->data = s;
-            }
-        }
-        else{
-            if (s[0] == '-'){
-                this->isNeg = true; 
-                this->data = s.substr(1); 
-            } 
-            else{
-                this->isNeg = false;
-                this->data = s;
-            }
-            reverse(data.begin(),data.end());
 
+public:
+    BigInteger(string s, bool rev_true = false) {
+        if (rev_true) {
+            int ss = s.size();
+            if (s[ss - 1] == '-') {
+                this->isNeg = true;
+                this->data = s.substr(0, ss - 1);
+            } else {
+                this->isNeg = false;
+                this->data = s;
+            }
+        } else {
+            if (s[0] == '-') {
+                this->isNeg = true;
+                this->data = s.substr(1);
+            } else {
+                this->isNeg = false;
+                this->data = s;
+            }
+            reverse(data.begin(), data.end());
         }
-        
     }
     BigInteger() : data("0"), isNeg(false) {}
 
-    BigInteger operator+(BigInteger& other){
-
-        if(this->isNegative() == other.isNegative()){
-
+    BigInteger operator+(const BigInteger& other) const {
+        if (this->isNegative() == other.isNegative()) {
             string result = addString(this->data, other.data);
-            if (this->isNegative())result = result + '-';
-            return BigInteger(result,1);
-        }
-        else{
-            if(absGreaterThan(this->data,other.data)){
+            if (this->isNegative()) result = result + '-';
+            return BigInteger(result, true);
+        } else {
+            if (absGreaterThan(this->data, other.data)) {
                 string result = subString(this->data, other.data);
-                if (this->isNegative())result = result + '-';
-                return BigInteger(result,1);
-
-            }
-            else{
-                
-                string result = subString(other.data,this->data);
-                if(other.isNegative())result = result + '-';
-                return BigInteger(result,1);
+                if (this->isNegative()) result = result + '-';
+                return BigInteger(result, true);
+            } else {
+                string result = subString(other.data, this->data);
+                if (other.isNegative()) result = result + '-';
+                return BigInteger(result, true);
             }
         }
     }
-    BigInteger operator-(BigInteger& other){
-
-        if(this->isNegative() != other.isNegative()){
-
+    BigInteger operator-(const BigInteger& other) const {
+        if (this->isNegative() != other.isNegative()) {
             string result = addString(this->data, other.data);
-            if (this->isNegative())result = result + '-';
-            return BigInteger(result,1);
-        }
-        else{
-            if(absGreaterThan(this->data,other.data)){
+            if (this->isNegative()) result = result + '-';
+            return BigInteger(result, true);
+        } else {
+            if (absGreaterThan(this->data, other.data)) {
                 string result = subString(this->data, other.data);
-                if (this->isNegative())result = result + '-';
-                return BigInteger(result,1);
-            }
-            else{
-                string result = subString(other.data,this->data);
-                if(other.isNegative())result = result + '-';
-                return BigInteger(result,1);
+                if (this->isNegative()) result = result + '-';
+                return BigInteger(result, true);
+            } else {
+                string result = subString(other.data, this->data);
+                if (!other.isNegative()) result = result + '-';
+                return BigInteger(result, true);
             }
         }
     }
     BigInteger operator*(const BigInteger& other) const {
         int sign = (this->isNeg == other.isNeg) ? 1 : -1;
         string result(this->data.size() + other.data.size(), '0');
-        
+
         for (int i = 0; i < this->data.size(); ++i) {
             int carry = 0;
             for (int j = 0; j < other.data.size(); ++j) {
@@ -168,17 +152,16 @@ public:
             }
             result[i + other.data.size()] += carry;
         }
-        
+
         while (result.size() > 1 && result.back() == '0') {
             result.pop_back();
         }
         if (sign == -1) result.push_back('-');
-        
-        return BigInteger(result,1);
+
+        return BigInteger(result, true);
     }
 
-
-    BigInteger& operator++() { 
+    BigInteger& operator++() {
         BigInteger one("1");
         *this = *this + one;
         return *this;
@@ -188,18 +171,17 @@ public:
         ++(*this);
         return temp;
     }
-    BigInteger& operator--() { 
+    BigInteger& operator--() {
         BigInteger one("-1");
         *this = *this + one;
         return *this;
     }
     BigInteger operator--(int) {
-
         BigInteger temp = *this;
         --(*this);
         return temp;
     }
-    
+
     bool operator==(const BigInteger& other) const {
         return this->isNeg == other.isNeg && this->data == other.data;
     }
@@ -230,6 +212,11 @@ public:
         return !(*this < other);
     }
 
+    BigInteger& operator+=(const BigInteger& other) {
+        *this = *this + other;
+        return *this;
+    }
+
     friend ostream& operator<<(ostream& os, const BigInteger& bi) {
         if (bi.isNeg) os << '-';
         for (int i = bi.data.size() - 1; i >= 0; --i) {
@@ -243,23 +230,13 @@ public:
         bi = BigInteger(input);
         return is;
     }
-
 };
 
-int main(){
+int main() {
+    BigInteger c("2");
+    BigInteger d("22222");
+    c = c*c*c*c*c*c*c;
     
-    BigInteger c;
-    BigInteger d;
-    cin>>c;
-    cin>>d;
-    cout<<c*d<<endl;
-    
-    
-
-
-   
-
-   
+    cout << c << endl;
     return 0;
-    
 }
